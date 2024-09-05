64831783b97b84a4169fbb9ed67db27856d1a213
Lilac0867 <116083750+Lilac0867@users.noreply.github.com>
2024-09-05 10:17:12 -0500

    Edit javascript.js to correct square count
    I mistook 16x16 for 16 squares, so I corrected the mistake.

--- javascript.js
+++ javascript.js
@@ -1,10 +1,10 @@
 const container = document.querySelector('#container');
 
-for(let i = 1; i <= 4; i++) {
+for(let i = 1; i <= 16; i++) {
     const row = document.createElement('div');
     row.classList.add('row');
 
-    for(let j = 1; j <= 4; j++) {
+    for(let j = 1; j <= 16; j++) {
         const square = document.createElement('div');
         square.classList.add('square');
         row.appendChild(square);
